45325eb6220f6960a20dd4e390f756f03a1cc74c
apurboarafat08-debug <apurboarafat08@gmail.com>
2026-06-21 18:17:44 +0600

    changes

--- js/custom.js
+++ js/custom.js
@@ -328,20 +328,4 @@ const swiper = new Swiper('.swiper', {
             slidesPerView: 3,
         }
     },
-
-        // If we need pagination
-        pagination: {
-            el: '.swiper-pagination',
-        },
-
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-
-        // And if we need scrollbar
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-    });
+});
